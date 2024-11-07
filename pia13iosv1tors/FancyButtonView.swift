2aeeac362b799200df5abe4f84148456407fc1f1
Bill Mårtensson <bill@magictechnology.se>
2024-11-07 12:35:30 +0100

    start

---
+++ pia13iosv1tors/FancyButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  FancyButtonView.swift
+//  pia13iosv1tors
+//
+//  Created by BillU on 2024-11-07.
+//
+
+import SwiftUI
+
+struct FancyButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FancyButtonView()
+}
